d3ac9e6bc6f994b59465f4246c0519684f71e31c
Shamonti <shamonti.haque98@gmail.com>
2024-06-06 15:16:40 +0600

    Added Django Rest Framework

--- api/models.py
+++ api/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Notes(models.Model):
+class Note(models.Model):
   body = models.TextField(null=True, blank=True)
   updated = models.DateTimeField(auto_now=True)
   created = models.DateTimeField(auto_now_add=True)
